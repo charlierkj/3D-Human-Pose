090571613c7b99114f09cd46a56934cbf35c7ac5
charlierkj <charlierkj@icloud.com>
2020-04-24 03:02:45 -0400

    2d pts re-aligned

--- cvt2format_hm.py
+++ cvt2format_hm.py
@@ -1,21 +1,42 @@
-import os, sys
+import os, sys, shutil
 import numpy as np
 import json
+import matplotlib.pyplot as plt
 
+
+joints_name = [
+        "foot_r",
+        "calf_r",
+        "thigh_r",
+        "thigh_l",
+        "calf_l",
+        "foot_l",
+        "pelvis",
+        "spine_02",
+        "head",
+        "head_end",
+        "hand_r",
+        "lowerarm_r",
+        "upperarm_r",
+        "upperarm_l",
+        "lowerarm_l",
+        "hand_l",
+        "neck_01"
+        ]
 
 def get_K(w, h, fov):
     """get 3x3 intrinsic matrix."""
     K = np.array([
         [fov, 0, w/2],
-        [0, fov, h/2],
+        [0, -fov, h/2],
         [0, 0, 1]
         ])
     return K
 
 def get_R(roll, pitch, yaw):
     """get 3x3 rotation matrix (camera w.r.t. world)."""
-    roll = roll / 180.0 * np.pi
-    pitch = pitch / 180.0 * np.pi
+    roll = -roll / 180.0 * np.pi
+    pitch = -pitch / 180.0 * np.pi
     yaw = yaw / 180.0 * np.pi
 
     Rroll = np.array([
@@ -53,7 +74,11 @@ def get_P(x, y, z, roll, pitch, yaw, w, h, fov):
     K = get_K(w, h, fov)
     R = get_R(roll, pitch, yaw)
     T = get_T(x, y, z)
-    P = K @ np.hstack((R.T, -R.T @ T))
+    ext_mat = np.array([
+        [0, 1, 0],
+        [0, 0, 1],
+        [1, 0, 0]]) @ np.hstack((R.T, -R.T @ T)) # extrinsic matrix (rows re-aligned)
+    P = K @ ext_mat
     return P
     
 
@@ -98,7 +123,7 @@ def generate_label(path):
         height = camera_dict['height']
         fov = camera_dict['fov']
 
-        K = get_K(width, height, fov) # intrinsics
+        K = get_K(width, height, width/2) # intrinsics
         R_c2w = get_R(rot[2], rot[0], rot[1])
         R_w2c = R_c2w.T # rotation
         T_c2w = get_T(pos[0], pos[1], pos[2])
@@ -106,8 +131,8 @@ def generate_label(path):
         
         camera_retval = retval['cameras'][0, camera_idx]
         camera_retval['K'] = K
-        camera_retval['R'] = R_w2c
-        camera_retval['t'] = T_w2c
+        camera_retval['R'] = np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0]]) @ R_w2c # rows re-aligned
+        camera_retval['t'] = np.array([[0, 1, 0], [0, 0, 1], [1, 0, 0]]) @ T_w2c # rows re-aligned
         camera_retval['dist'] = np.zeros(5)
 
     # hardcode bounding boxes
@@ -115,26 +140,6 @@ def generate_label(path):
     bboxes = np.tile(bbox, (len(retval['camera_names']), 1))
 
     # fill retval['table']
-    joints_name = [
-        "foot_r",
-        "calf_r",
-        "thigh_r",
-        "thigh_l",
-        "calf_l",
-        "foot_l",
-        "pelvis",
-        "spine_02",
-        "head",
-        "head_end",
-        "hand_r",
-        "lowerarm_r",
-        "upperarm_r",
-        "upperarm_l",
-        "lowerarm_l",
-        "hand_l",
-        "neck_01"
-        ]
-
     for f in os.listdir(data_path):
         f_path = os.path.join(data_path, f)
         if not os.path.isdir(f_path):
@@ -145,12 +150,13 @@ def generate_label(path):
             skeleton = json.load(json_file)
 
         all_joints = {joint["Name"]: joint["KpWorld"] for joint in skeleton}
+        num_jnts = len(joints_name)
         x_keypts = np.array([all_joints[jnt]['X'] for jnt in joints_name])
         y_keypts = np.array([all_joints[jnt]['Y'] for jnt in joints_name])
         z_keypts = np.array([all_joints[jnt]['Z'] for jnt in joints_name])
-        keypoints = np.hstack((x_keypts.reshape(17,1),\
-                               y_keypts.reshape(17,1),\
-                               z_keypts.reshape(17,1)))
+        keypoints = np.hstack((x_keypts.reshape(num_jnts,1),\
+                               y_keypts.reshape(num_jnts,1),\
+                               z_keypts.reshape(num_jnts,1)))
             
         table_segment = np.empty(1, dtype=table_dtype)
         table_segment['subject_idx'] = 0
@@ -169,12 +175,59 @@ def generate_label(path):
     return
 
 def reorganize_imgs(path):
-    return
+    data_path = os.path.join(path, "multiview_data")
+    write_path = os.path.join(path, "processed", "S1", "Standing", "imageSequence-undistorted")
+    os.makedirs(write_path, exist_ok=True)
+    for i in range(1, 5):
+        if not os.path.exists(os.path.join(write_path, "%d" % i)):
+            os.mkdir(os.path.join(write_path, "%d" % i))
 
+    for f in os.listdir(data_path):
+        f_path = os.path.join(data_path, f)
+        if not os.path.isdir(f_path):
+            continue
 
+        dest_filename = "img_%06d.jpg" % (int(f) + 1)
+        for i in range(1, 5):
+            src_path = os.path.join(f_path, "view_%d.png" % i)
+            dest_path = os.path.join(write_path, "%d" % i, dest_filename)
+            shutil.copy(src_path, dest_path)
+
+def plot_joints_2D(img_path, skeleton_file, camera_file):
+    with open(skeleton_file) as skeleton_json:
+        skeleton = json.load(skeleton_json)
+
+    all_joints = {joint["Name"]: joint["KpWorld"] for joint in skeleton}
+    num_jnts = len(joints_name)
+    X = np.array([all_joints[jnt]['X'] for jnt in joints_name]).reshape(1,num_jnts)
+    Y = np.array([all_joints[jnt]['Y'] for jnt in joints_name]).reshape(1,num_jnts)
+    Z = np.array([all_joints[jnt]['Z'] for jnt in joints_name]).reshape(1,num_jnts)
+    pts_3d = np.vstack((X, Y, Z)) # 3 x n
+    pts_3d_homo = np.vstack((pts_3d, np.ones((1,num_jnts)))) # 4 x n    
+
+    with open(camera_file) as camera_json:
+        camera = json.load(camera_json)
+
+    proj_mat = get_P(camera['location'][0], camera['location'][1], camera['location'][2],
+                     camera['rotation'][2], camera['rotation'][0], camera['rotation'][1],
+                     camera['width'], camera['height'], camera['width'] / 2) # 3 x 4
+    pts_2d_homo = proj_mat @ pts_3d_homo # 3 x n
+    pts_2d = np.zeros((2, pts_2d_homo.shape[1])) # 2 x n
+    pts_2d[0, :] = pts_2d_homo[0, :] / pts_2d_homo[2, :]
+    pts_2d[1, :] = pts_2d_homo[1, :] / pts_2d_homo[2, :]
+
+    img = plt.imread(img_path)
+    plt.imshow(img)
+    plt.scatter(pts_2d[0, :], pts_2d[1, :])
+    plt.show()
+
+    
 if __name__ == "__main__":
     ds_path = "data/multiview_data"
     generate_label(ds_path)
-    
-    
+    reorganize_imgs(ds_path)
+
+    plot_joints_2D("data/multiview_data/multiview_data/000000/view_2.png",
+                   "data/multiview_data/multiview_data/000000/skeleton.json",
+                   "data/multiview_data/multiview_data/camera_2.json")   
     
