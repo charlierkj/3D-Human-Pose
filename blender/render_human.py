01abeb6468d941f42aff3c9fb42bde6f2ecd30ee
charlierkj <charlierkj@icloud.com>
2020-06-28 05:34:55 -0400

    render human initial commit

---
+++ blender/render_human.py
@@ -0,0 +1,64 @@
+import os, sys, subprocess
+import numpy as np
+import bpy
+
+blend_filepath = bpy.data.filepath
+blend_dir = os.path.split(blend_filepath)[0]
+print(blend_dir)
+sys.path.append(blend_dir)
+
+import tf_utils
+
+
+def render_human(bvh_file, viewpoint, img_size, save_folder):
+    """cam_pose: 3-tuple: (distance, azimuth, elevation)."""
+    if save_folder != '':
+        os.makedirs(save_folder, exist_ok=True)
+
+    # add bvh file and set animation
+    bpy.ops.import_anim.bvh(filepath=bvh_file, filter_glob='*.bvh', \
+                            frame_start=1, axis_forward='X', axis_up='Z')
+    bvh_filename = os.path.basename(bvh_file)
+    anim_name = os.path.splitext(bvh_filename)[0]
+    skeleton_obj = bpy.data.objects['originalPose']
+    skeleton_obj.animation_data.action = bpy.data.actions.get(anim_name)
+    bpy.data.objects[anim_name].select_set(True)
+    bpy.ops.object.delete()
+
+    bpy.context.scene.frame_set(0)
+
+    # place camera
+    camera_obj = bpy.data.objects['Camera']
+    camera_loc, camera_rot_euler = tf_utils.spherical_to_pose(viewpoint)
+    camera_rot_quaternion = tf_utils.euler_to_quaternion(camera_rot_euler)
+    camera_obj.location = camera_loc
+    camera_obj.rotation_mode = 'QUATERNION'
+    camera_obj.rotation_quaternion = camera_rot_quaternion
+
+    # render
+    # set resolution
+    (bpy.context.scene.render.resolution_x, bpy.context.scene.render.resolution_y) = img_size
+    
+    for frame_idx in range(2, 61): # hardcoded, need later modification
+        bpy.context.scene.frame_set(frame_idx)
+        img_path = os.path.join(save_folder, '%06d.jpg' % (frame_idx - 1))
+        bpy.data.scenes['Scene'].render.filepath = img_path
+        bpy.ops.render.render(write_still=True)       
+    
+    
+def blender_cmd(bvh_file, cam_pose, save_folder):
+
+    blender_path = 'C:/Users/charl/Work/Blender Foundation/Blender 2.83/blender.exe' # absolute path for blender.exe
+    blend_file = 'prototype_0522.blend' # .blend file
+    py_file = 'npy2bvh.py'
+
+    render_cmd = f'"{blender_path}" "{blend_file}" --python "{py_file}"'
+    print('Run blender command: %s' % render_cmd)
+    subprocess.run(render_cmd)
+
+
+if __name__ == "__main__":
+    bvh_file = './bvh/anim_100.bvh'
+    save_folder = 'C:/Users/charl/Work/hopkins_time/Research/learnable-triangulation-pytorch/blender/bvh/anim_100'
+
+    render_human(bvh_file, (2, 135, 30), (640, 480), save_folder)
--- blender/tf_utils.py
+++ blender/tf_utils.py
@@ -1,4 +1,7 @@
 import numpy as np
+import math
+
+PI = math.pi
 
 def skew_symm_mat(w):
     """hat-operation,
@@ -26,5 +29,36 @@ def compute_rotation(vec_a, vec_b):
     rot_mat = np.eye(3) + sin_theta * w_hat + \
               (1 - cos_theta) * (w_hat @ w_hat) # Rodrigue's formula
     return rot_mat
+
+def spherical_to_pose(viewpoint):
+    """assuming camera aimed to origin, compute 6DOF camera pose.
+    viewpoint: 3-tuple, (distance, azimuth, elevation)
+    """
+    dist, az, el = viewpoint
+    phi = el / 180 * PI
+    theta = az / 180 * PI
+    x = dist * np.cos(theta) * np.cos(phi)
+    y = dist * np.sin(theta) * np.cos(phi)
+    z = dist * np.sin(phi)
+    roll = - el
+    pitch = 0
+    yaw = - az + PI
+    return [x, y, z], [roll, pitch, yaw]
+
+def euler_to_quaternion(euler):
+    roll, pitch, yaw = euler
+    c1 = np.cos(yaw / 2.0)
+    c2 = np.cos(pitch / 2.0)
+    c3 = np.cos(roll / 2.0)    
+    s1 = np.sin(yaw / 2.0)
+    s2 = np.sin(pitch / 2.0)
+    s3 = np.sin(roll / 2.0)    
+    qw = c1 * c2 * c3 + s1 * s2 * s3
+    qx = c1 * c2 * s3 - s1 * s2 * c3
+    qy = c1 * s2 * c3 + s1 * c2 * s3
+    qz = s1 * c2 * c3 - c1 * s2 * s3
+    return [qw, qx, qy, qz]
+
+
     
     
