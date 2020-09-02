73f839eed88c3f3be2b08cce4d79dafd630be9f5
charlierkj <charlierkj@icloud.com>
2020-09-02 02:20:05 -0400

    fix

---
+++ test.py
@@ -0,0 +1,255 @@
+import os, json
+import argparse
+import pickle
+from collections import defaultdict
+import numpy as np
+import torch
+from PIL import Image
+
+from utils import cfg
+from utils.eval import *
+from models.triangulation import AlgebraicTriangulationNet
+from models.loss import PCK, KeypointsL2Loss
+from datasets.multiview_syndata import MultiView_SynData
+from datasets.human36m import Human36MMultiViewDataset
+import datasets.utils as datasets_utils
+
+import utils.visualize as visualize
+
+import train
+import utils.eval as utils_eval
+
+
+def test_one_epoch(model, val_loader, metric, device):
+    model.eval()
+    with torch.no_grad():
+        total_samples = 0
+        total_error = 0
+        total_detected = 0
+        for iter_idx, (images_batch, proj_mats_batch, joints_3d_gt_batch, joints_3d_valid_batch, info_batch) in enumerate(val_loader):
+            if images_batch is None:
+                continue
+                    
+            images_batch = images_batch.to(device)
+            proj_mats_batch = proj_mats_batch.to(device)
+            joints_3d_gt_batch = joints_3d_gt_batch.to(device)
+            joints_3d_valid_batch = joints_3d_valid_batch.to(device)
+            batch_size = images_batch.shape[0]
+            joints_3d_pred, joints_2d_pred, heatmaps_pred, confidences_pred = model(images_batch, proj_mats_batch)
+
+            detected, error, num_samples = utils_eval.eval_one_batch(metric, joints_3d_pred, joints_2d_pred, \
+                                                                     proj_mats_batch, joints_3d_gt_batch, joints_3d_valid_batch)
+
+            total_detected += detected
+            total_error += num_samples * error
+            total_samples += num_samples
+
+        pck_acc = total_detected / total_samples # 2D
+        mean_error = total_error / total_samples # 3D
+
+    return pck_acc, mean_error
+
+
+def syndata_test(model, dataloader, device, save_folder, show_img=False, make_gif=False, make_vid=False):
+    frames_per_scene = 60
+    
+    model.to(device)
+    model.eval()
+
+    #scene_names = []
+    subj_names = []
+    anim_names = []
+    metrics = {}
+    with torch.no_grad():
+        for iter_idx, (images_batch, proj_mats_batch, joints_3d_gt_batch, joints_3d_valid_batch, info_batch) in enumerate(dataloader):
+            print(iter_idx)
+            if iter_idx >= 30:
+                break
+
+            if images_batch is None:
+                continue
+
+            images_batch = images_batch.to(device)
+            proj_mats_batch = proj_mats_batch.to(device)
+            joints_3d_gt_batch = joints_3d_gt_batch.to(device)
+            joints_3d_valid_batch = joints_3d_valid_batch.to(device)
+
+            batch_size = images_batch.shape[0]
+            
+            joints_3d_pred, joints_2d_pred, heatmaps_pred, confidences_pred = model(images_batch, proj_mats_batch)
+
+            [subj_name, anim_idx, frame] = info_batch[0]
+            if frame == 0:
+                anim_name = 'anim_%03d' % anim_idx
+                print(subj_name, anim_name)
+                if subj_name not in subj_names:
+                    subj_names.append(subj_name)
+                if anim_name not in anim_names:
+                    anim_names.append(anim_name)
+                
+                joints_3d_pred_np = np.empty([0,] + list(joints_3d_pred.detach().cpu().numpy().shape[1::]))
+                joints_2d_pred_np = np.empty([0,] + list(joints_2d_pred.detach().cpu().numpy().shape[1::]))
+                heatmaps_pred_np = np.empty([0,] + list(heatmaps_pred.detach().cpu().numpy().shape[1::]))
+                confidences_pred_np = np.empty([0,] + list(confidences_pred.detach().cpu().numpy().shape[1::]))
+
+                preds_folder = os.path.join(save_folder, 'preds', subj_name, anim_name)
+                os.makedirs(preds_folder, exist_ok=True)
+
+            joints_3d_pred_np = np.concatenate((joints_3d_pred_np, joints_3d_pred.detach().cpu().numpy()), axis=0)
+            joints_2d_pred_np = np.concatenate((joints_2d_pred_np, joints_2d_pred.detach().cpu().numpy()), axis=0)
+            heatmaps_pred_np = np.concatenate((heatmaps_pred_np, heatmaps_pred.detach().cpu().numpy()), axis=0)
+            confidences_pred_np = np.concatenate((confidences_pred_np, confidences_pred.detach().cpu().numpy()), axis=0)
+
+            if (frame + batch_size) == frames_per_scene:
+                # save intermediate results
+                print('saving intermediate results...')
+                np.save(os.path.join(preds_folder, 'joints_3d.npy'), joints_3d_pred_np) # numpy array of size (num_frames, num_joints, 3)
+                np.save(os.path.join(preds_folder, 'joints_2d.npy'), joints_2d_pred_np) # numpy array of size (num_frames, num_views, num_joints, 2)
+                #np.save(os.path.join(preds_folder, 'heatmaps.npy'), heatmaps_pred_np) # numpy array of size (num_frames, num_views, num_joints, 120, 120)
+                np.save(os.path.join(preds_folder, 'confidences.npy'), confidences_pred_np) # numpy array of size (num_frames, num_views, num_joints)
+
+    # save evaluations and visualizations
+    for subj_name in subj_names:
+        metrics_subj = {}
+        for anim_name in anim_names:
+            scene_folder = os.path.join(dataloader.dataset.basepath, subj_name, anim_name) # subj currently hardcoded
+            if not os.path.exists(scene_folder):
+                continue
+
+            # evaluate
+            joints_3d_pred_path = os.path.join(save_folder, 'preds', subj_name, anim_name, 'joints_3d.npy')
+            joints_2d_pred_path = os.path.join(save_folder, 'preds', subj_name, anim_name, 'joints_2d.npy')
+            error_per_scene = evaluate_one_scene(joints_3d_pred_path, scene_folder, invalid_joints=(9, 16))
+            metrics_subj[anim_name] = error_per_scene
+
+            # save images
+            print('saving result images...')
+            imgs_folder = os.path.join(save_folder, 'imgs', subj_name, anim_name)
+            visualize.draw_one_scene(joints_3d_pred_path, joints_2d_pred_path, scene_folder, imgs_folder, show_img=show_img)
+
+            # save gifs/videos (optioanl)
+            if make_gif:
+                print('saving result gifs...')
+                gif_name = os.path.join(save_folder, 'gifs', subj_name, '%s.gif' % anim_name)
+                visualize.make_gif(imgs_folder, gif_name)
+
+            if make_vid:
+                print('saving result videos...')
+                vid_name = os.path.join(save_folder, 'vids', subj_name, '%s.mp4' % anim_name)
+                visualize.make_vid(imgs_folder, vid_name)
+
+        metrics[subj_name] = metrics_subj
+            
+    # save evaluation
+    print('saving evaluation results...')
+    metrics_path = os.path.join(save_folder, 'metrics.json')
+    with open(metrics_path, 'w') as metrics_json:
+        json.dump(metrics, metrics_json)
+
+
+def human36m_test(model, dataloader, device, save_folder, show_img=False, make_gif=False, make_vid=False):
+    os.makedirs(save_folder, exist_ok=True)
+    saveimg_per_iter = 10
+    
+    model.to(device)
+    model.eval()
+
+    preds = defaultdict(list)
+
+    with torch.no_grad():
+        for iter_idx, (images_batch, proj_mats_batch, joints_3d_gt_batch, joints_3d_valid_batch, indexes) in enumerate(dataloader):
+            print(iter_idx)
+            if iter_idx >= 60:
+                break
+
+            if images_batch is None:
+                continue
+
+            images_batch = images_batch.to(device)
+            proj_mats_batch = proj_mats_batch.to(device)
+            joints_3d_gt_batch = joints_3d_gt_batch.to(device)
+            joints_3d_valid_batch = joints_3d_valid_batch.to(device)
+
+            batch_size = images_batch.shape[0]
+            
+            joints_3d_pred, joints_2d_pred, heatmaps_pred, confidences_pred = model(images_batch, proj_mats_batch)
+
+            preds["indexes"].append(indexes)
+            preds["joints_3d"].append(joints_3d_pred.detach().cpu().numpy())
+            preds["joints_2d"].append(joints_2d_pred.detach().cpu().numpy())
+            preds["confidences"].append(confidences_pred.detach().cpu().numpy())
+
+            # save images
+            imgs_folder = os.path.join(save_folder, "imgs")
+            os.makedirs(imgs_folder, exist_ok=True)
+            if iter_idx % saveimg_per_iter == 0:
+                img = visualize.visualize_pred(images_batch[0], proj_mats_batch[0], joints_3d_gt_batch[0], joints_3d_pred[0], joints_2d_pred[0])
+                im = Image.fromarray(img)
+                img_path = os.path.join(imgs_folder, "%06d.png" % indexes[0])
+                im.save(img_path)
+
+    preds["indexes"] = np.concatenate(preds["indexes"])
+    preds["joints_3d"] = np.concatenate(preds["joints_3d"], axis=0)
+    preds["joints_2d"] = np.concatenate(preds["joints_2d"], axis=0)
+    preds["confidences"] = np.concatenate(preds["confidences"], axis=0)
+
+    print('saving prediction results...')
+    preds_path = os.path.join(save_folder, "preds.pkl")
+    with open(preds_path, 'wb') as pkl_file:
+        pickle.dump(preds, pkl_file)
+
+
+    
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default="experiments/syndata/test/syndata_alg_17jnts.yaml")
+    args = parser.parse_args()
+
+    config = cfg.load_config(args.config)
+
+    assert config.dataset.type in ("syndata", "human36m")
+
+    device = torch.device(int(config.gpu_id))
+
+    model = AlgebraicTriangulationNet(config, device=device).to(device)
+
+    if config.model.init_weights:
+        model = train.load_pretrained_model(model, config)
+    
+    print("Loading data..")
+
+    if args.data == "syndata":
+        dataset = MultiView_SynData(config.dataset.data_root, load_joints=config.model.backbone.num_joints, invalid_joints=(9, 16), \
+                                    bbox=config.dataset.bbox, image_shape=config.dataset.image_shape)
+        dataloader = datasets_utils.syndata_loader(dataset, \
+                                                   batch_size=config.dataset.test.batch_size, \
+                                                   shuffle=config.dataset.test.shuffle, \
+                                                   num_workers=config.dataset.test.num_workers)
+
+        save_folder = os.path.join(os.getcwd(), 'results/mocap_syndata_%djnts' % config.model.backbone.num_joints)
+        #save_folder = os.path.join(os.getcwd(), 'results/mocap_syndata')
+        syndata_test(model, dataloader, device, save_folder, make_vid=False)
+
+    elif args.data == "human36m":
+        dataset = Human36MMultiViewDataset(
+                    h36m_root=config.dataset.data_root,
+                    test=True,
+                    image_shape=config.dataset.image_shape,
+                    labels_path=config.dataset.labels_path,
+                    with_damaged_actions=config.dataset.test.with_damaged_actions,
+                    retain_every_n_frames_in_test=config.dataset.test.retain_every_n_frames_in_test,
+                    scale_bbox=config.dataset.test.scale_bbox,
+                    kind="human36m",
+                    undistort_images=config.dataset.test.undistort_images,
+                    ignore_cameras=config.dataset.test.ignore_cameras if hasattr(config.dataset.val, "ignore_cameras") else [],
+                    crop=config.dataset.test.crop if hasattr(config.dataset.val, "crop") else True,
+                )
+        dataloader = datasets_utils.human36m_loader(dataset, \
+                                                    batch_size=config.dataset.test.batch_size, \
+                                                    shuffle=config.dataset.test.shuffle, \
+                                                    num_workers=config.dataset.test.num_workers)
+
+        save_folder = os.path.join(os.getcwd(), 'results/human36m_%djnts' % config.model.backbone.num_joints)
+        human36m_test(model, dataloader, device, save_folder, make_vid=False)
+
---
+++ train.py
@@ -0,0 +1,279 @@
+import os
+import numpy as np
+import argparse
+import torch
+from torch import nn
+from datetime import datetime
+from PIL import Image
+import yaml
+
+from tensorboardX import SummaryWriter
+
+from utils import cfg
+from models.triangulation import AlgebraicTriangulationNet
+from models.loss import HeatmapMSELoss, PCK, KeypointsMSELoss, KeypointsMSESmoothLoss, KeypointsMAELoss, KeypointsL2Loss
+from datasets.multiview_syndata import MultiView_SynData
+import datasets.utils as datasets_utils
+import utils.visualize as visualize
+import test
+import utils.eval as utils_eval
+
+
+def load_pretrained_model(model, config, init_joints=17):
+    device = next(model.parameters()).device
+    model_state_dict = model.state_dict()
+
+    pretrained_state_dict = torch.load(config.model.checkpoint)
+    for key in list(pretrained_state_dict.keys()):
+        new_key = key.replace("module.", "")
+        pretrained_state_dict[new_key] = pretrained_state_dict.pop(key)
+
+    new_pretrained_state_dict = {}
+    for k, v in pretrained_state_dict.items():
+        if k in model_state_dict and v.shape == model_state_dict[k].shape:
+            new_pretrained_state_dict[k] = v
+        elif k == "backbone.alg_confidences.head.4.weight":
+            print("Reiniting alg_confidences layer filters:", k)
+            o = torch.zeros_like(model_state_dict[k][:, :])
+            nn.init.xavier_uniform_(o)
+            n_filters = min(o.shape[0], v.shape[0], init_joints)
+            o[:n_filters, :] = v[:n_filters, :]
+            new_pretrained_state_dict[k] = o
+        elif k == "backbone.alg_confidences.head.4.bias":
+            print("Reiniting alg_confidences layer biases:", k)
+            o = torch.zeros_like(model_state_dict[k][:])
+            nn.init.zeros_(o)
+            n_filters = min(o.shape[0], v.shape[0], init_joints)
+            o[:n_filters] = v[:n_filters]
+            new_pretrained_state_dict[k] = o
+        elif k == "backbone.final_layer.weight":  # TODO
+            print("Reiniting final layer filters:", k)
+            o = torch.zeros_like(model_state_dict[k][:, :, :, :])
+            nn.init.xavier_uniform_(o)
+            n_filters = min(o.shape[0], v.shape[0], init_joints)
+            o[:n_filters, :, :, :] = v[:n_filters, :, :, :]
+            new_pretrained_state_dict[k] = o
+        elif k == "backbone.final_layer.bias":
+            print("Reiniting final layer biases:", k)
+            o = torch.zeros_like(model_state_dict[k][:])
+            nn.init.zeros_(o)
+            n_filters = min(o.shape[0], v.shape[0], init_joints)
+            o[:n_filters] = v[:n_filters]
+            new_pretrained_state_dict[k] = o
+
+    not_inited_params = set(pretrained_state_dict.keys()) - set(new_pretrained_state_dict.keys())
+    if len(not_inited_params) > 0:
+        print("Parameters [{}] were not inited".format(not_inited_params))
+
+    model.load_state_dict(new_pretrained_state_dict, strict=True)
+    print("Successfully loaded pretrained weights for whole model")
+    return model
+
+
+def train_one_epoch(model, train_loader, criterion, metric, opt, e, device, \
+                    checkpoint_dir, writer=None, log_every_iters=1):
+    model.train()
+    batch_size = train_loader.batch_size
+    iters_per_epoch = round(total_samples / batch_size)
+    print("Estimated iterations per epoch is %d." % iters_per_epoch)
+    total_train_loss = 0
+    total_samples = 0
+
+    sum_loss_per_log = 0
+    sum_detected_per_log = 0
+    sum_error_per_log = 0
+    sum_samples_per_log = 0 # num_joints or num_frames
+
+    for iter_idx, (images_batch, proj_mats_batch, joints_3d_gt_batch, joints_3d_valid_batch, info_batch) in enumerate(train_loader):
+        if images_batch is None:
+            continue
+
+        images_batch = images_batch.to(device)
+        proj_mats_batch = proj_mats_batch.to(device)
+        joints_3d_gt_batch = joints_3d_gt_batch.to(device)
+        joints_3d_valid_batch = joints_3d_valid_batch.to(device)
+
+        batch_size = images_batch.shape[0]
+
+        joints_3d_pred, joints_2d_pred, heatmaps_pred, confidences_pred = model(images_batch, proj_mats_batch)
+
+        # use predictions of invalid joints as groundtruth
+        #joints_clone = ~(torch.squeeze(joints_3d_valid_batch, 2).type(torch.bool))
+        #joints_3d_gt_batch[joints_clone] = joints_3d_pred[joints_clone].detach().clone()
+        #joints_all_valid = torch.ones_like(joints_3d_valid_batch)
+
+        # calculate loss
+        if isinstance(criterion, HeatmapMSELoss):
+            loss = criterion(heatmaps_pred, proj_mats_batch, joints_3d_gt_batch, joints_3d_valid_batch)
+        else:
+            loss = criterion(joints_3d_pred, joints_3d_gt_batch, joints_3d_valid_batch)
+
+        # optimize
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+
+        # evaluate batch
+        with torch.no_grad():
+            detected, error, num_samples = utils_eval.eval_one_batch(metric, joints_3d_pred, joints_2d_pred, \
+                                                                     proj_mats_batch, joints_3d_gt_batch, joints_3d_valid_batch)
+            sum_samples_per_log += num_samples
+            sum_loss_per_log += num_samples * loss.item()
+            sum_detected_per_log += detected
+            sum_error_per_log += num_samples * error
+
+            total_train_loss += num_samples * loss.item()
+            total_samples += num_samples
+            
+        if iter_idx % log_every_iters == log_every_iters - 1:
+            logging_iter = iter_idx + 1 - log_every_iters
+            mean_loss_per_log = sum_loss_per_log / sum_samples_per_log
+            pck_acc_per_log = sum_detected_per_log / sum_samples_per_log
+            mean_error_per_log = sum_error_per_log / sum_samples_per_log
+            print("epoch: %d, iter: %d, train loss: %.3f, train acc: %.3f, train error: %.3f" \
+                  % (e, logging_iter, mean_loss_per_log, pck_acc_per_log, mean_error_per_log))
+
+            if writer is not None:
+                writer.add_scalar("train loss (iter)", mean_loss_per_log, e * iters_per_epoch + logging_iter)
+                writer.add_scalar("train pck (iter)", pck_acc_per_log, e * iters_per_epoch + logging_iter)
+                writer.add_scalar("train error (iter)", mean_error_per_log, e * iters_per_epoch + logging_iter)
+
+            sum_samples_per_log = 0
+            sum_loss_per_log = 0
+            sum_detected_per_log = 0
+            sum_error_per_log = 0
+
+    # save training loss per epoch to tensorboard
+    mean_loss = total_train_loss / total_samples
+    if writer is not None:
+        writer.add_scalar("train loss (epoch)",  mean_loss, e)
+
+    # evaluate using training set
+    #with torch.no_grad():
+    #    model.eval()
+    #    pck_acc, mean_error = test.test_one_epoch(model, train_loader, metric, device)
+    #    if writer is not None:
+    #        writer.add_scalar("train pck (epoch)", pck_acc, e)
+    #        writer.add_scalar("train error (epoch)", mean_error, e)
+        
+    return mean_loss, pck_acc, mean_error
+    
+
+def multiview_train(config, model, train_loader, val_loader, criterion, opt, epochs, device, \
+                    log_every_iters=1, \
+                    resume=False, logdir="./logs", exp_log="exp_27jnts@15.08.2020-05.15.16"):
+    # configure dir and writer for saving weights and intermediate evaluation
+    if not resume:
+        experiment_name = "{}_{}jnts@{}".format("exp", "%d" % config.model.backbone.num_joints, datetime.now().strftime("%d.%m.%Y-%H.%M.%S"))
+        start_epoch = 0
+    else:
+        experiment_name = exp_log
+        start_epoch = int(sorted(os.listdir(os.path.join(logdir, experiment_name, "checkpoint")))[-1]) + 1
+        
+    experiment_dir = os.path.join(logdir, experiment_name)
+    os.makedirs(experiment_dir, exist_ok=True)
+    checkpoint_dir = os.path.join(experiment_dir, "checkpoint")
+    os.makedirs(checkpoint_dir, exist_ok=True)
+    writer = SummaryWriter(os.path.join(experiment_dir, "tensorboard"))
+    yaml_path = os.path.join(experiment_dir, "param.yaml")
+
+    param_dict = {}
+    param_dict["dataset"] = config.dataset.type
+    param_dict["train_batch_size"] = config.dataset.train.batch_size
+    param_dict["test_batch_size"] = config.dataset.test.batch_size
+    param_dict["image_shape"] = config.dataset.image_shape
+    param_dict["num_joints"] = config.model.backbone.num_joints
+    param_dict["backbone_num_layers"] = config.model.backbone.num_layers
+    param_dict["criterion"] = config.opt.criterion
+    param_dict["opt_lr"] = config.opt.lr
+    param_dict["epochs"] = config.opt.n_epochs
+    with open(yaml_path, 'w') as f:
+        data = yaml.dump(param_dict, f)
+    
+    model.to(device)
+
+    if isinstance(criterion, HeatmapMSELoss):
+        metric = PCK()
+    else:
+        metric = KeypointsL2Loss()
+
+    for e in range(start_epoch, epochs):
+        # train for one epoch
+        train_loss, train_acc, train_error = train_one_epoch(model, train_loader, criterion, metric, opt, e, device, \
+                                                             checkpoint_dir, writer, log_every_iters)
+
+        # evaluate
+        test_acc, test_error = test.test_one_epoch(model, val_loader, metric, device)
+        
+        writer.add_scalar("test pck (epoch)", test_acc, e)
+        writer.add_scalar("test error (epoch)", test_error, e)
+        
+        print('Epoch: %03d | Train Loss: %.3f | Train Acc: %.3f | Train Error: %.3f | Test Acc: %.3f | Test Error: %.3f' \
+              % (e, train_loss, train_acc, train_error, test_acc, test_error))
+
+        # save weights
+        with torch.no_grad():
+            checkpoint_dir_e = os.path.join(checkpoint_dir, "%04d" % e)
+            os.makedirs(checkpoint_dir_e, exist_ok=True)
+            torch.save(model.state_dict(), os.path.join(checkpoint_dir_e, "weights.pth"))
+
+    # save weights
+    # torch.save(model.state_dict(), os.path.join(checkpoint_dir, "weights.pth"))
+    print("Training Done.")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default="experiments/syndata/test/syndata_alg_17jnts.yaml")
+    parser.add_argument('--resume', action='store_true')
+    parser.add_argument('--logdir', type=str, default="./logs")
+    args = parser.parse_args()
+
+    config = cfg.load_config(args.config)
+
+    device = torch.device(int(config.gpu_id))
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+
+    model = AlgebraicTriangulationNet(config, device=device)
+
+    model = torch.nn.DataParallel(model, device_ids=[int(config.gpu_id)])
+
+    if config.model.init_weights:
+        print("Initializing model weights..")
+        model = load_pretrained_model(model, config)
+
+    # load data
+    print("Loading data..")
+    train_set = MultiView_SynData(config.dataset.data_root, load_joints=config.model.backbone.num_joints, invalid_joints=(), \
+                                  bbox=config.dataset.bbox, image_shape=config.dataset.image_shape, \
+                                  train=True)
+    train_loader = datasets_utils.syndata_loader(train_set, \
+                                                 batch_size=config.dataset.train.batch_size, \
+                                                 shuffle=config.dataset.train.shuffle, \
+                                                 num_workers=config.dataset.train.num_workers)
+
+    val_set = MultiView_SynData(config.dataset.data_root, load_joints=config.model.backbone.num_joints, invalid_joints=(), \
+                                bbox=config.dataset.bbox, image_shape=config.dataset.image_shape, \
+                                test=True)
+    val_loader = datasets_utils.syndata_loader(val_set, \
+                                               batch_size=config.dataset.test.batch_size, \
+                                               shuffle=config.dataset.test.shuffle, \
+                                               num_workers=config.dataset.test.num_workers)
+
+    # configure loss
+    if config.opt.criterion == "MSESmooth":
+        criterion = KeypointsMSESmoothLoss(config.opt.mse_smooth_threshold)
+    elif config.opt.criterion == "MSE":
+        criterion = KeypointsMSELoss()
+    elif config.opt.criterion == "MAE":
+        criterion = KeypointsMAELoss()
+    elif config.opt.criterion == "Heatmap":
+        criterion = HeatmapMSELoss(config.dataset.image_shape)
+
+    # configure optimizer
+    opt = torch.optim.Adam(filter(lambda p : p.requires_grad, model.parameters()), lr=config.opt.lr)
+
+    multiview_train(config, model, train_loader, val_loader, criterion, opt, config.opt.n_epochs, device, \
+                    resume=args.resume, logdir=args.logdir)
